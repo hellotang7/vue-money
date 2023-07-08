4a9b4e17fcb13f509e1bd027dd819995f0101686
hellotang7 <2714148252@qq.com>
2023-07-08 17:08:00 +0800

    初始化3个页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,35 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from 'vue';
+import VueRouter, {RouteConfig} from 'vue-router';
+import Hello from '../components/HelloWorld.vue';
+import Money from '@/views/Money.vue';
+import Labels from '@/views/Labels.vue';
+import Statistics from '@/views/Statistics.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+
+    {
+        path: '/',
+        redirect:'/money'
+    }, {
+        path: '/money',
+        component: Money
+    }, {
+        path: '/labels',
+        component: Labels
+    }, {
+        path: '/statistics',
+        component: Statistics
+    }, {
+        path: '/hello',
+        component: Hello
+    },
+
+];
 
 const router = new VueRouter({
-  routes
-})
+    routes
+});
 
-export default router
+export default router;
