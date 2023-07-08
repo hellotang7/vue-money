83b7368d5becb49a3c3e3b19ebd8f41892ddd498
hellotang7 <2714148252@qq.com>
2023-07-08 17:12:51 +0800

    全局引入<Nav/>

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
+    <div id="app">
 
-    <router-view/>
-      <div>
-          <router-link to="/money" active-class="x">记账|</router-link>
-          <router-link to="/labels" active-class="x">标签|</router-link>
-          <router-link to="/statistics" active-class="x">统计</router-link>
-      </div>
-  </div>
+        <router-view/>
+        <Nav/>
+    </div>
 </template>
+<script>
 
+
+</script>
 <style lang="scss">
-.x{
-  color: #42b983;
-}
+
 </style>
