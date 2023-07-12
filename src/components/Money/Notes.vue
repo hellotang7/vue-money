1f6b34eee4a5efa8938ed56709491f79ab460763
hellotang7 <2714148252@qq.com>
2023-07-12 14:26:47 +0800

    完成notes组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -2,7 +2,7 @@
     <div>
         <label class="notes">
             <span class="name">备注：</span>
-            <input type="text" placeholder="在这里输入备注">
+            <input v-model="value" type="text" placeholder="在这里输入备注">
         </label>
     </div></template>
 
@@ -11,9 +11,8 @@
     import {Component} from 'vue-property-decorator';
 
     @Component
-
     export default class Notes extends Vue {
-
+    value='';
 
 
     }
